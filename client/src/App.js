fc53d1d5ac75c8eec031a2c2a8db3e0ac0a4816f
a3vezes <tucaariza@gmail.com>
2021-04-03 17:04:01 -0300

    Frontend, Router, ContactContext and ContactForm

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './componenents/layout/Navbar'
 import Home from './componenents/pages/Home'
 import About from './componenents/pages/About'
+import ContactState from './context/contact/ContactState'
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <div className='container'>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-        </Switch>
-      </div>
-    </Router>
+    <ContactState>
+      <Router>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    </ContactState>
   )
 }
 
